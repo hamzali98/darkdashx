64c6a946506c4d4d7c56d23be79ad3c498928ea5
Hamza Ali <hamza.ali.art3@gmail.com>
2025-12-04 10:10:45 +0500

    generic table.
    New generic table.

--- src/app/features/users/viewusers/viewusers.ts
+++ src/app/features/users/viewusers/viewusers.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { GenericTable } from '@app/shared/components/generic-table/generic-table';
 
 @Component({
   selector: 'app-viewusers',
-  imports: [],
+  imports: [GenericTable],
   templateUrl: './viewusers.html',
   styleUrl: './viewusers.css',
 })
