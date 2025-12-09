a4628fd770bd8162b823e59d8fb3b750ff4b1b36
Hamza Ali <hamza.ali.art3@gmail.com>
2025-12-09 22:10:25 +0500

    added loader over app.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,20 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Loaderservice } from './core/services/loader/loaderservice';
+import { Loader } from "./core/component/loader/loader";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Loader],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('dashboard');
+
+   private loaderService = inject(Loaderservice);
+
+  get loaderState (){
+    return this.loaderService.getLoader();
+  }
 }
