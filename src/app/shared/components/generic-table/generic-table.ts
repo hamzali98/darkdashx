f30686ff360ec4dee9af421757c9de5abeceef5e
Hamza Ali <hamza.ali.art3@gmail.com>
2025-12-16 23:02:24 +0500

    pagination added

--- src/app/shared/components/generic-table/generic-table.ts
+++ src/app/shared/components/generic-table/generic-table.ts
@@ -1,21 +1,26 @@
-import { Component, NgModule, EventEmitter, input, Input, OnInit, Output, signal } from '@angular/core';
+import { Component, EventEmitter, input, Input, Output, signal, OnChanges, SimpleChanges } from '@angular/core';
 import { NgClass } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { tableColumns } from '@app/shared/interface/generic-table-interface';
-import { User } from '@app/features/users/interface/user';
+import { TitleCasePipe, CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-generic-table',
-  imports: [FormsModule, NgClass],
+  imports: [FormsModule, NgClass, TitleCasePipe, CurrencyPipe],
   templateUrl: './generic-table.html',
   styleUrl: './generic-table.css',
 })
-export class GenericTable<T> {
+export class GenericTable<T> implements OnChanges {
 
   checked = signal(true);
   status = signal(false);
 
-  itemsPerPage: number = 10;
+  itemsPerPage: number = 5;
+  currentPage: number = 1;
+  startIndex: number = 0;
+  endIndex: number = 0;
+
+  currentPageData: T[] = [];
 
   tableName = input("Generic");
 
@@ -28,12 +33,20 @@ export class GenericTable<T> {
   @Output() onDeleteClicked: EventEmitter<any> = new EventEmitter();
   @Output() onEditClicked: EventEmitter<any> = new EventEmitter();
 
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['tableData']) {
+      this.currentPage = 1;
+      this.updatePagedData();
+    }
+  }
+
   get tableTotal() {
     return this.tableData?.length ?? 0;
   }
 
   get isIndeterminate() {
-    if (this.checkList.length === 0 || this.checkList.length === this.tableData.length) {
+    // if (this.checkList.length === 0 || this.checkList.length === this.tableData.length) {
+    if (this.checkList.length === 0 || this.checkList.length === this.currentPageData.length) {
       return false;
     } else {
       return true;
@@ -67,26 +80,23 @@ export class GenericTable<T> {
     console.log(this.checkList);
   }
 
-  onCheckboxChange(event: any) {
-    // console.log(event.target.id);
-    console.log(`checked : ${event.target.checked} , id : ${event.target.id}`);
-  }
+  // onCheckboxChange(event: any) {
+  //   // console.log(event.target.id);
+  //   console.log(`checked : ${event.target.checked} , id : ${event.target.id}`);
+  // }
 
   toggleSelectallRows(event: any) {
     const checked = event.target.checked;
     if (checked) {
-      this.checkList = [...this.tableData];
+      // this.checkList = [...this.tableData];
+      this.checkList = [...this.currentPageData]
     } else {
       this.checkList = [];
     }
 
     console.log("whole check list : ", this.checkList);
   }
-
-  onChangePerPage() {
-    console.log(this.itemsPerPage);
-  }
-
+  
   getValue(obj: any, key: any) {
     return key.reduce((access: any, key: any) => access?.[key], obj);
     // return key.split('.').reduce((access:any, key:any) => access?.[key], obj);
@@ -96,9 +106,46 @@ export class GenericTable<T> {
     // console.log(data);
     this.onDeleteClicked.emit(data);
   }
-
+  
   onClickedEdit(data: T) {
     this.onEditClicked.emit(data);
   }
+
+  get displayStartIndex() {
+    return this.tableData ? this.startIndex + 1 : this.startIndex;
+  }
+
+  onChangePerPage() {
+    console.log(this.itemsPerPage);
+    this.updatePagedData();
+  }
+
+  updatePagedData() {
+    this.startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    this.endIndex = this.startIndex + this.itemsPerPage;
+    if (this.endIndex > this.tableTotal) {
+      this.endIndex = this.tableTotal;
+    }
+    this.currentPageData = this.tableData.slice(this.startIndex, this.endIndex);
+  }
+
+  previousPage() {
+    console.log('prev button clicked');
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.updatePagedData();
+    }
+  }
+
+  nextPage() {
+    console.log('next button clicked');
+    const totalpages = this.tableData.length / this.itemsPerPage;
+    if (this.currentPage < totalpages) {
+      this.currentPage++;
+      this.updatePagedData();
+    }
+  }
+
+
 }
 
