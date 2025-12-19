f647db898a3af1fa44e04265a7157685fc25402a
Hamza Ali <hamza.ali.art3@gmail.com>
2025-12-19 10:21:49 +0500

    login pages designs

--- src/app/core/auth/pages/login-page/login-page.ts
+++ src/app/core/auth/pages/login-page/login-page.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
+import { FormGroup, ReactiveFormsModule, FormsModule, FormControl, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login-page',
-  imports: [],
+  imports: [ReactiveFormsModule, FormsModule],
   templateUrl: './login-page.html',
   styleUrl: './login-page.css',
 })
 export class LoginPage {
+
+loginForm : FormGroup;
+
+constructor(){
+  this.loginForm = new FormGroup({
+    email : new FormControl("", Validators.required),
+    password : new FormControl("", Validators.required),
+  });
+  this.loginForm.markAllAsTouched();
+}
+
+get email (){
+  return this.loginForm.get("email");
+}
+
+get password () {
+  return this.loginForm.get("password")
+}
+
+onLoginSubmit(){
+  
+}
 
 }
--- src/app/core/auth/pages/signup/signup.ts
+++ src/app/core/auth/pages/signup/signup.ts
@@ -1,11 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Httpservice } from '@app/shared/services/httpservice/httpservice';
+import { Loaderservice } from '@app/shared/services/loader/loaderservice';
+import { SnackBarService } from '@app/shared/services/snackbar/snack-bar-service';
+import { environment } from '@environments/environment.development';
 
 @Component({
   selector: 'app-signup',
-  imports: [],
+  imports: [ReactiveFormsModule, FormsModule],
   templateUrl: './signup.html',
   styleUrl: './signup.css',
 })
 export class Signup {
+
+  passmatch = signal(false);
+
+  AuthURL : string = environment.AUTH_URL;
+
+  private httpService = inject(Httpservice);
+  private loaderService = inject(Loaderservice);
+  private snackService = inject(SnackBarService);
+
+  signupForm: FormGroup;
+
+  constructor() {
+    this.signupForm = new FormGroup({
+      username: new FormControl("", [Validators.required, Validators.maxLength(15), Validators.minLength(6)]),
+      email: new FormControl("", [Validators.required, Validators.email]),
+      password: new FormControl("", [Validators.required, Validators.minLength(6)]),
+      c_password: new FormControl("", Validators.required),
+    });
+    this.signupForm.markAllAsTouched();
+  }
+
+  get username() {
+    return this.signupForm.get("username");
+  }
+
+  get email() {
+    return this.signupForm.get("email");
+  }
+  get password() {
+    return this.signupForm.get("password");
+  }
+
+  get c_password() {
+    return this.signupForm.get("c_password");
+  }
+
+  // checkPassStrength() {
+  // }
+
+  checkPassMatch() {
+    this.passmatch.set(this.password?.value !== this.c_password?.value ? true : false);
+    // return this.password?.value !== this.c_password?.value ? true : false;
+  }
+
+  onSignupSubmit(){
+    this.loaderService.showLoader();
+    this.httpService.addApi(this.AuthURL, this.signupForm.value).subscribe({
+      next: (res) => {
+        console.log(res);
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    })
+  }
 
 }
