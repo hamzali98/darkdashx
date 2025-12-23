b62eb3db3977d43aa1021786138108f8a868a20b
Hamza Ali <hamza.ali.art3@gmail.com>
2025-12-23 11:07:34 +0500

    main logo moved in component

---
+++ src/app/shared/components/main-logo/main-logo.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-main-logo',
+  imports: [RouterLink],
+  templateUrl: './main-logo.html',
+  styleUrl: './main-logo.css',
+})
+export class MainLogo {
+
+}
