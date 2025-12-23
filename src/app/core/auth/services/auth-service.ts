dc40f75150a2288beb3b57b11895e5247f6bae96
Hamza Ali <hamza.ali.art3@gmail.com>
2025-12-23 11:05:10 +0500

    auth service enhanced

--- src/app/core/auth/services/auth-service.ts
+++ src/app/core/auth/services/auth-service.ts
@@ -1,31 +1,56 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { credentials } from '../interface/credentials';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   private readonly USER_KEY = 'user';
-  private readonly REMEMBER_ME = 'remember';
+
+  private readonly EXPIRY_KEY = 'session_expiry';
+  private readonly SESSION_DURATION = 60 * 1000;
+  // private readonly REMEMBER_ME = 'remember';
+
+  private routerRef = inject(Router);
 
   // Save user after login
-  login(user: credentials): void {
+  login(user: credentials, remember: boolean): void {
+
+    const expiryTime = remember 
+    ? Date.now() + 7 * 24 * 60 * 60 * 1000 
+    : Date.now() + this.SESSION_DURATION;
+
     localStorage.setItem(this.USER_KEY, JSON.stringify(user));
+    // localStorage.setItem(this.REMEMBER_ME, remember.toString());
+    localStorage.setItem(this.EXPIRY_KEY, expiryTime.toString());
   }
 
-  rememberMe(){
-    localStorage.setItem(this.REMEMBER_ME, "true");
-  }
+  // rememberMe() {
+  //   localStorage.setItem(this.REMEMBER_ME, remember);
+  // }
 
   // Remove user on logout
   logout(): void {
     localStorage.removeItem(this.USER_KEY);
-    localStorage.removeItem(this.REMEMBER_ME);
+    localStorage.removeItem(this.EXPIRY_KEY);
+    // localStorage.removeItem(this.REMEMBER_ME);
+    this.routerRef.navigate(['/login']);
   }
 
   // Check if user exists
   isAuthenticated(): boolean {
-    return !!localStorage.getItem(this.USER_KEY);
+    const user = localStorage.getItem(this.USER_KEY);
+    const expiry = localStorage.getItem(this.EXPIRY_KEY);
+
+    if (!user || !expiry) return false;
+
+    if (Date.now() > Number(expiry)) {
+      this.logout(); // auto logout
+      return false;
+    }
+
+    return true;
   }
 
   // Get logged-in user data
