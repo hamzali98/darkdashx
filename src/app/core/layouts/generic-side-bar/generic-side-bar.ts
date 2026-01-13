22a692cc71e6c81170176c027794fd7aaa046bcf
Hamza Ali <hamza.ali.art3@gmail.com>
2026-01-13 14:19:20 +0500

    removed junk

--- src/app/core/layouts/generic-side-bar/generic-side-bar.ts
+++ src/app/core/layouts/generic-side-bar/generic-side-bar.ts
@@ -14,33 +14,19 @@ import { Layout } from '@app/core/services/layout';
 })
 export class GenericSideBar<T> implements OnInit {
 
-  // sideBarOpen = signal(true);
-  // open = signal<string | null>(null);
-
   @Input() navData: sidenavcols<T>[] = [];
 
   private routerRef = inject(Router);
-  // private authService = inject(AuthService);
   private layoutService = inject(Layout);
-  // constructor() {}
+  constructor() { }
 
   ngOnInit() {
     console.log("ng on init");
     const route = this.routerRef.routerState.snapshot.url.toString();
     console.log('active route', route);
-    const rout : any = route.split('/').at(1);
+    const rout: any = route.split('/').at(1);
     console.log(rout);
-    // this.onOpen(rout?.toString());
     this.layoutService.onOpen(rout.toString());
-  //   // if (route.startsWith('/users')) {
-  //   //   this.onOpen('users');
-  //   // } else if (route.startsWith('/settings')) {
-  //   //   this.onOpen('settings');
-  //   // } else if (route.startsWith('/profile')) {
-  //   //   this.onOpen('profile');
-  //   // } else {
-  //   //   this.onOpen('dashboard');
-  //   // }
   }
 
   get open() {
@@ -51,47 +37,25 @@ export class GenericSideBar<T> implements OnInit {
     return this.layoutService.username;
   }
 
-  // get username(){
-  //   return this.authService.getUser()?.username ?? "Guest";
-  // }
+  get sidebar() {
+    return this.layoutService.getSidebarState();
+  }
 
   openAndNavigate(section: string, route: string) {
     this.layoutService.openAndNavigate(section, route);
-  //   this.onOpen(section);
-  //   this.routerRef.navigate([route]);
   }
 
-  // onOpen(section: string) {
-  //   if (section === '') {
-  //     this.open.set('home');
-  //   } else {
-
-  //     if (section === this.open()) {
-  //       this.open.set(null);
-  //     } else {
-  //       this.open.set(section);
-  //     }
-  //   }
-  //   // this.open.update(v => !v);
-  // }
+  toggleSidebar() {
+    this.layoutService.toggleSidebar();
+  }
 
   onRoute(route: string) {
     this.layoutService.onRoute(route);
-  //   this.routerRef.navigate([route]);
-  //   this.onOpen(route);
   }
 
   isActive(route: string): boolean {
     return this.layoutService.isActive(route);
-  //   if (route === '' || route === '/') {
-  //     return this.routerRef.url === '/' || this.routerRef.url === '';
-  //   }
-  //   return this.routerRef.url.includes(route);
   }
-
-  // onSidebarclick() {
-  //   this.sideBarOpen.update(v => !v)
-  // }
 
   onLogout() {
     this.layoutService.onLogout();
