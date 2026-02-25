00fe719337ac7bfb8289e98b64f1df4d830890bc
Hamza Ali <hamza.ali.art3@gmail.com>
2026-02-25 11:57:19 +0500

    under development screen added.

--- src/app/features/dashboard/reports/reports.ts
+++ src/app/features/dashboard/reports/reports.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { UnderDevelopment } from "@app/shared/components/under-development/under-development";
 
 @Component({
   selector: 'app-reports',
-  imports: [],
+  imports: [UnderDevelopment],
   templateUrl: './reports.html',
   styleUrl: './reports.css',
 })
--- src/app/features/dashboard/tasks/tasks.ts
+++ src/app/features/dashboard/tasks/tasks.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { UnderDevelopment } from "@app/shared/components/under-development/under-development";
 
 @Component({
   selector: 'app-tasks',
-  imports: [],
+  imports: [UnderDevelopment],
   templateUrl: './tasks.html',
   styleUrl: './tasks.css',
 })
