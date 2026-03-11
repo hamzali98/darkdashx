6fe4a61f62f06642f78e00edb3beb48a689d9ef2
Hamza Ali <hamza.ali.art3@gmail.com>
2026-03-11 11:48:46 +0500

    cleaned

--- src/app/shared/components/search-bar/search-bar.ts
+++ src/app/shared/components/search-bar/search-bar.ts
@@ -1,10 +1,11 @@
 import { Component, model } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
+import { CustomInputConfig, GenericInput } from "../generic-input/generic-input";
 
 @Component({
   selector: 'app-search-bar',
-  imports: [FormsModule, TranslateModule],
+  imports: [FormsModule, TranslateModule, GenericInput],
   templateUrl: './search-bar.html',
   styleUrl: './search-bar.css',
 })
@@ -12,6 +13,27 @@ import { TranslateModule } from '@ngx-translate/core';
 export class SearchBar {
 
   searchKey = model("");
+
+  searchbarConfig: CustomInputConfig;
+
+  constructor() {
+    this.searchbarConfig = {
+      ngclass: '',
+      startIcon: 'assets/icons/neutral/search.svg',
+      type: 'text',
+      inputId: 'search',
+      inputName: 'search',
+      errorMessage: '',
+      placeholder: 'SEARCH',
+      iconActions: [
+        {
+          iconPath: 'assets/icons/input_icons/cross.svg',
+          action: () => this.clearText(),
+          isActive: () => !!this.searchKey(), 
+        }
+      ]
+    }
+  }
 
   updateSearchKey(key: string | null | undefined) {
     this.searchKey.set(key || "");
