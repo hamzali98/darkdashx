72172e7cc22b301851bf002c9295158071839ab9
Hamza Ali <hamza.ali.art3@gmail.com>
2026-05-14 10:33:28 +0500

    .

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,16 +1,22 @@
+const BASE_URL = "http://localhost:3000";
+
 export const environment = {
   production: false,
-  BASE_URL: "http://localhost:3000",
+  // BASE_URL: "http://localhost:3000",
 
-  get USER_URL() {
-    return `${this.BASE_URL}/users`;
-  },
+  // get USER_URL() {
+  //   return `${this.BASE_URL}/users`;
+  // },
 
-  get PRODUCTS_URL() {
-    return `${this.BASE_URL}/products`;
-  },
+  // get PRODUCTS_URL() {
+  //   return `${this.BASE_URL}/products`;
+  // },
 
-  get AUTH_URL() {
-    return `${this.BASE_URL}/credentials`
-  }
+  // get AUTH_URL() {
+  //   return `${this.BASE_URL}/credentials`
+  // }
+
+  USER_URL: `${BASE_URL}/users`,
+  PRODUCTS_URL: `${BASE_URL}/products`,
+  AUTH_URL: `${BASE_URL}/credentials`,
 };
